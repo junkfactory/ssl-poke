1090934d24c324013f63afaaa305631b73efad88
amontecillo <amontecillo@marqeta.com>
2016-07-05 16:58:10 -0700

    Initial commit

---
+++ src/main/java/com/marqeta/util/App.java
@@ -0,0 +1,44 @@
+package com.marqeta.util;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if (args.length != 2) {
+            System.out.println((new StringBuilder()).append("Usage: ").append(App.class.getName()).append(" <host> <port>").toString());
+            System.exit(1);
+        }
+        try {
+            if (new App().poke(args[0], Integer.parseInt(args[1]))) {
+                System.out.println("Successfully connected");
+            }
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public boolean poke(String host, int port) throws IOException {
+
+        SSLSocketFactory sslsocketfactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
+        SSLSocket sslsocket = (SSLSocket)sslsocketfactory.createSocket(host, port);
+        sslsocket.setEnabledProtocols(new String[] {
+                "SSLv3", "TLSv1", "TLSv1.2"
+        });
+        InputStream in = sslsocket.getInputStream();
+        OutputStream out = sslsocket.getOutputStream();
+        out.write(1);
+        for(; in.available() > 0; System.out.print(in.read()));
+        return true;
+
+    }
+}
---
+++ src/test/java/com/marqeta/util/AppTest.java
@@ -0,0 +1,40 @@
+package com.marqeta.util;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.IOException;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() throws IOException {
+        App app = new App();
+        app.poke("marqeta.com", 443);
+    }
+}
